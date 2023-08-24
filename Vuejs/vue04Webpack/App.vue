a946f85dac01e8b0b9c33c1d2c122d309f6861fd
dbkim <dabin1004i@naver.com>
2023-08-24 11:36:32 +0900

    2023-08-24

--- Vuejs/vue04Webpack/App.vue
+++ Vuejs/vue04Webpack/App.vue
@@ -1,4 +1,3 @@
-
 <style scoped>
 section {
   height: 150px;
@@ -7,7 +6,9 @@ section {
   margin: 40px 0px;
 }
 
-div {margin: 40px 20px;}
+div {
+  margin: 40px 20px;
+}
 </style>
 
 <template>
@@ -15,7 +16,7 @@ div {margin: 40px 20px;}
     <header-comp></header-comp>
 
     <section id="page1" data-role="page">
-        <div class="content" data-role="content">컨텐츠</div>
+      <div class="content" data-role="content">컨텐츠</div>
     </section>
 
     <footer-comp></footer-comp>
@@ -23,20 +24,18 @@ div {margin: 40px 20px;}
 </template>
 
 <script>
-import Header from "./Header.vue";
-import Footer from "./Footer.vue";
+import Header from './HeaderComp.vue';
+import Footer from './FooterComp.vue';
 
 export default {
   data() {
     return {
-      msg: "Hello VueJS",
+      msg: 'Hello VueJS',
     };
   },
   components: {
-    "header-comp": Header,
-    "footer-comp": Footer,
+    'header-comp': Header,
+    'footer-comp': Footer,
   },
 };
 </script>
-
-// <!-- 컴포넌트의 제일 큰 특징은 어디를 가든 수정이 없어야 하는 개념으로 바뀐것 -->
